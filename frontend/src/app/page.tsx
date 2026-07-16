11c7db955c266a26e609236d0185549fe2d10329
JOAO BERNARDELLI <joaobernardelli123@gmail.com>
2026-07-16 17:01:37 -0300

    redirecionamento de tela

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useMachines } from "@/modules/machine/hooks/use-machines";
-
-export default function MachinesPage() {
-  const result = useMachines({
-    page: 1,
-    pageSize: 10,
-  });
-
-  console.log(result);
-
-  return (
-    <div>
-      <pre>{JSON.stringify({
-        status: result.status,
-        isPending: result.isPending,
-        isLoading: result.isLoading,
-        isFetching: result.isFetching,
-        error: result.error,
-      }, null, 2)}</pre>
-    </div>
-  );
+export default function HomePage() {
+  redirect("/dashboard");
 }
